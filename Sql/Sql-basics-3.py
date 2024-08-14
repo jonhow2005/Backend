d4c73b11f032f5bdacbcc8c96d70bc4100790547
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-08-13 21:19:12 -0400

    Create Sql-basics-3.py
    Uploading information to a database.

---
+++ Sql/Sql-basics-3.py
@@ -0,0 +1,22 @@
+import sqlite3
+ 
+# connecting to the database
+connection = sqlite3.connect("gfg.db")
+
+crsr = connection.cursor()
+ 
+# insert data
+sql_command = """INSERT INTO tbl VALUES (00, "value1",\
+"value2", " ", "2000-01-01");"""
+crsr.execute(sql_command)
+ 
+# alter data to be inserted and insert again
+sql_command = """INSERT INTO tbl VALUES (01, "value1, 2nd", "value2, 2nd",\
+"", "1980-10-28");"""
+crsr.execute(sql_command)
+ 
+# save change to database
+connection.commit()
+ 
+# close connection
+connection.close()
