c2d8e4856c684346dcaf8484d27909871bad0cca
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-08-13 21:24:02 -0400

    Update Sql-basics-3.py
    Fix

--- Sql/Sql-basics-3.py
+++ Sql/Sql-basics-3.py
@@ -1,17 +1,17 @@
 import sqlite3
  
 # connecting to the database
-connection = sqlite3.connect("gfg.db")
+connection = sqlite3.connect("filename.db")
 
 crsr = connection.cursor()
  
 # insert data
-sql_command = """INSERT INTO tbl VALUES (00, "value1",\
+sql_command = """INSERT INTO tbl VALUES (0, "value1",\
 "value2", " ", "2000-01-01");"""
 crsr.execute(sql_command)
  
 # alter data to be inserted and insert again
-sql_command = """INSERT INTO tbl VALUES (01, "value1, 2nd", "value2, 2nd",\
+sql_command = """INSERT INTO tbl VALUES (1, "value1 2nd", "value2 2nd",\
 "", "1980-10-28");"""
 crsr.execute(sql_command)
  
