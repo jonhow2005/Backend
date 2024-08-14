77cfadb5f44b8cf6092d82dda803d7e99c285023
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-08-13 22:17:26 -0400

    Update Sql-basics-3.py

--- Sql/Sql-basics-3.py
+++ Sql/Sql-basics-3.py
@@ -1,14 +1,15 @@
 import sqlite3
- 
+
+idname = str("ninganote")
 # connecting to the database
 connection = sqlite3.connect("filename.db")
 
 crsr = connection.cursor()
  
 # insert data
-sql_command = """INSERT INTO tbl VALUES (0, "value1",\
+sql_command = """INSERT INTO tbl VALUES (0, "?",\
 "value2", " ", "2000-01-01");"""
-crsr.execute(sql_command)
+crsr.execute(sql_command(idname))
  
 # alter data to be inserted and insert again
 sql_command = """INSERT INTO tbl VALUES (1, "value1 2nd", "value2 2nd",\
