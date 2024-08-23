84282fd86dbc16f50b89270946b40fd7c1bde8b0
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-08-22 23:41:10 -0400

    Update and rename Sql4.py to Sqlite-basics_4.py

--- Sql/Sqlite-basics_4.py
+++ Sql/Sqlite-basics_4.py
@@ -2,20 +2,20 @@ import sqlite3
   
 # Connecting to sqlite 
 # connection object 
-connection_obj = sqlite3.connect('geek.db') 
+connection_obj = sqlite3.connect('imgeekin.db') 
 
 # cursor object 
 cursor_obj = connection_obj.cursor() 
   
 # to select all column we will use 
-statement = '''SELECT * FROM GEEK'''
+statement = '''SELECT * FROM GEEKER'''
   
 cursor_obj.execute(statement) 
 Selection = Str(input("All/Custom Amount/One"))  
 print(Selection, "data set(s)") 
 if Selection == All:
     output = cursor_obj.fetchall()
-elif Selection = "one":
+elif Selection == "one":
     output = cursor_obj.fetchmany(Selection) 
 else:
     output = cursor_obj.fetchone() 
