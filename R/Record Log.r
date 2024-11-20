2140a46817c65ca99d8f9a2c85b2ccaaa6269900
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-11-20 17:49:28 -0500

    Update Record Log.r

--- R/Record Log.r
+++ R/Record Log.r
@@ -1,15 +1,41 @@
 "Options:"
 "New Record/ Acsess Log/ Clear"
-DayArray <- list[]
-MonthArray <- list("")
-YearArray <- list("")
-LogArray <- list("")
+resp <- readln("")
+Array1 <- list("--")
+Array2 <- list("--")
+Array3 <- list("----")
+LogArray <- list("\n")
 
+New_Record <- function(){
 Data1 <- scan("Scan Day")
 Data2 <- scan("Scan Month")
 Data3 <- scan("Scan Year")
 Record <- readln("Enter Log")
 
-DayArray.append
-MonthArray.append
-YearArray.append
+Array1 <- append(Array1, Data1)
+Array2 <- append(Array2, Data2)
+Array3 <- append(Array3, Data3)
+LogArray <- append(LogArray, Record)
+}
+
+Acsess_Log <- function(){
+for x in LogArray{
+  print(paste(Array1[x],"/",Array2[x],"/",Array3[x]))
+  print(LogArray[x])
+}
+}
+
+Clear <- function(){
+  cat("/014")
+}
+
+if (resp = "Clear") {
+  Clear()
+}else if (resp = "New Record"){
+  New_Record()
+}
+
+else if (resp = "Acsess Log"){
+  Acsess_Log()
+}
+  
