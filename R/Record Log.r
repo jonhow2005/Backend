27d40f04024a505456ab905a8b64e74c0c9c6e16
Jonathan Howard <110829395+jonhow2005@users.noreply.github.com>
2024-11-20 15:48:20 -0500

    Update Record Log.r

--- R/Record Log.r
+++ R/Record Log.r
@@ -1,8 +1,15 @@
 "Options:"
 "New Record/ Acsess Log/ Clear"
-
-DateArray <- list("")
+DayArray <- list[]
+MonthArray <- list("")
+YearArray <- list("")
 LogArray <- list("")
 
-Date <- ""
-Record <- ""
+Data1 <- scan("Scan Day")
+Data2 <- scan("Scan Month")
+Data3 <- scan("Scan Year")
+Record <- readln("Enter Log")
+
+DayArray.append
+MonthArray.append
+YearArray.append
